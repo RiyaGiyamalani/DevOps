ac268e29a77dd773c5cbb241a9f314aa2bd09eb5
Riya Giyamalani <riyagiyamalani@outlook.com>
2026-08-05 05:48:52 +0000

    test2

--- test.js
+++ test.js
@@ -1,29 +1,37 @@
 const fs = require("fs");
 
 if (fs.existsSync("index.html")) {
-    console.log(" index.html exists");
+    console.log("index.html exists");
 } else {
-    console.log(" index.html not found");
-    process.exit(1);
+    console.log("index.html not found");
 }
 
 if (fs.existsSync("style.css")) {
-    console.log(" style.css exists");
+    console.log("style.css exists");
 } else {
-    console.log(" style.css not found");
-    process.exit(1);
+    console.log("style.css not found");
 }
 
 if (fs.existsSync("script.js")) {
-    console.log(" script.js exists");
+    console.log("script.js exists");
 } else {
     console.log("script.js not found");
-    process.exit(1);
 }
 
 if (fs.existsSync("students.json")) {
-    console.log(" students.json exists");
+    console.log("students.json exists");
+
+    const students = JSON.parse(fs.readFileSync("students.json", "utf8"));
+
+    let passed = true;
+
+    if (students.name.trim() !== "") {
+        console.log("TC : Name Validation : PASS");
+    } else {
+        console.log("TC : Name Validation : FAIL");
+        passed = false;
+    }
+
 } else {
-    console.log(" students.json not found");
-    process.exit(1);
+    console.log("students.json not found");
 }
